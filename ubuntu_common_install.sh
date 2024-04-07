6cd842a5c46f3a70280c4df132ed647ad6cd6196
zsdfbb <zsdfbb@126.com>
2024-04-07 23:36:39 +0800

    add ubuntu server configuration
    Signed-off-by: Jack <zsdfbb@126.com>

--- ubuntu_common_install.sh
+++ ubuntu_common_install.sh
@@ -5,6 +5,9 @@ echo "Start install common software"
 sudo apt update
 sudo apt install tmux vim fzf fd-find git git-lfs \
     ripgrep curl wget tree unzip gcc make python3-venv python3-pip \
-    shfmt 
+    shfmt net-tools
+
+# configuration ubuntu server network
+# sudo ifconfig enp0s8 192.168.56.110 netmask 255.255.255.0
 
 echo "Common software is installed"
