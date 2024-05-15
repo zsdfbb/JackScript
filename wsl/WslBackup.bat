34ca528e72605b82e5310442b82cbce8ed2bb305
zsdfbb <zsdfbb@126.com>
2024-05-15 23:51:21 +0800

    Add wsl script
    Signed-off-by: zsdfbb <zsdfbb@126.com>

---
+++ wsl/WslBackup.bat
@@ -0,0 +1,2 @@
+wsl --shutdown
+wsl --export ubuntu-24.04 D:\\WslBackup\\ubuntu-24.04.tar
---
+++ wsl/WslImport.bat
@@ -0,0 +1,3 @@
+wsl --shutdown
+wsl --import Ubuntu-24.04 D:\\WSL D:\\WslBackup\\ubuntu-24.04.tar --version 2
+ubuntu2404 config --default-user zs
