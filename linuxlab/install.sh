726f22cb614b355c87ca46e8d74be85158db8508
zsdfbb <zsdfbb@126.com>
2024-05-19 17:00:36 +0800

    fix path error of linuxlab script Signed-off-by: Jack <zsdfbb@126.com>

--- linuxlab/install.sh
+++ linuxlab/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# BASE_DIR 是仓的linuxlab目录
 BASE_DIR=$(pwd)
+
 LAB_DIR=${HOME}/Develop/LinuxLab
 mkdir -p $LAB_DIR
 
 #=================
 # 安装依赖
 #=================
-
 sudo apt install qemu-system-aarch64 qemu-user \
 	qemu-user-static gcc-aarch64-linux-gnu \ 
 	binutils-aarch64-linux-gnu binutils-aarch64-linux-gnu-dbg \ 
@@ -21,15 +22,13 @@ mkdir -p $LAB_DIR/busybox \
 	$LAB_DIR/busybox/build \
 	$LAB_DIR/busybox/src \
 	$LAB_DIR/busybox/install
-cp $BASE_DIR/linuxlab/busybox_build.sh $LAB_DIR/busybox/build.sh
+cp $BASE_DIR/busybox_build.sh $LAB_DIR/busybox/build.sh
 
 mkdir -p $LAB_DIR/linux \
 	$LAB_DIR/linux/build \
 	$LAB_DIR/linux/src \
 	$LAB_DIR/linux/install
-cp $BASE_DIR/linuxlab/linux_build.sh $LAB_DIR/busybox/build.sh
-
-
+cp $BASE_DIR/linux_build.sh $LAB_DIR/busybox/build.sh
 
 #=================
 # 准备 rootfs
