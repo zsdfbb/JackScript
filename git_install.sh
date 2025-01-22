6f4ac0821d3502f03567e8bed1fdc7348130539c
zs <zsdfbb@126.com>
2025-01-22 14:45:44 +0800

    add git_ignore
    Signed-off-by: Jack <zsdfbb@126.com>

--- git_install.sh
+++ git_install.sh
@@ -4,6 +4,7 @@ echo "Start configure git"
 rm -rf ${HOME}/.gitconfig ${HOME}/.git_template
 cp $(pwd)/git/gitconfig ${HOME}/.gitconfig
 cp $(pwd)/git/git_template ${HOME}/.git_template
+cp $(pwd)/git/git_ignore ${HOME}/.git_ignore
 
 git config --global --unset http.proxy
 git config --global --unset https.proxy
