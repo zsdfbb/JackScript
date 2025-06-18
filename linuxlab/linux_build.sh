57fc9e5528d88a3d686d84e3724a751a66573626
zs <zsdfbb@126.com>
2025-06-18 11:52:24 +0800

    update linux build
    Signed-off-by: Jack <zsdfbb@126.com>

--- linuxlab/linux_build.sh
+++ linuxlab/linux_build.sh
@@ -21,6 +21,12 @@ fi
 if [ $1 = "build" ]; then
     cd $BUILD
     make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+    # 生成clangd依赖的编译选项，使用vscode阅读代码有帮助
+    # 参考：
+    # https://blog.csdn.net/ludaoyi88/article/details/135051470
+    # https://blog.csdn.net/ytfrdfiw/article/details/138954978
+    ./source/scripts/clang-tools/gen_compile_commands.py ./
+    cp compile_commands.json ../src/
 fi
 
 if [ $1 = "clean" ]; then
