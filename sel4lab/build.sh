87538633dee02577faec144b3779aff00f30a3ae
Zhang Shuai <zsdfbb@126.com>
2025-07-14 11:32:08 +0800

    fix(build.sh): add CMAKE_EXPORT_COMPILE_COMMANDS option to init-build.sh for both architectures

--- sel4lab/build.sh
+++ sel4lab/build.sh
@@ -24,7 +24,7 @@ if [ $ARCH = "x86" ]; then
     rm -rf ./seL4test/build-x86/
     mkdir -p ./seL4test/build-x86/
     cd ./seL4test/build-x86/
-    ../init-build.sh -DPLATFORM=x86_64 -DSIMULATION=TRUE
+    ../init-build.sh -DPLATFORM=x86_64 -DSIMULATION=TRUE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
     ninja
 fi
 
@@ -32,7 +32,7 @@ if [ $ARCH = "aarch64" ]; then
     rm -rf ./seL4test/build_aarch64/
     mkdir -p ./seL4test/build_aarch64/
     cd ./seL4test/build_aarch64/
-    ../init-build.sh -DPLATFORM=qemu-arm-virt -DSIMULATION=TRUE
+    ../init-build.sh -DPLATFORM=qemu-arm-virt -DSIMULATION=TRUE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
     ninja
     # sed -i 's#/host/#/home/zs/Develop/SeL4/#g' compile_commands.json
 fi
