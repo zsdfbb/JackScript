22d8b25eb600f4796e52d84e66e2423b7ccce77b
Zhang Shuai <zsdfbb@126.com>
2026-06-03 20:34:06 +0800

    fix: Delete error pack
    Signed-off-by: Zhang Shuai <zsdfbb@126.com>

--- nvim_install.sh
+++ nvim_install.sh
@@ -29,37 +29,8 @@ if [ $CMD = "conf" ]; then
     tar -xf ~/OpenSrc/nerd-fonts/SourceCodePro.tar.xz -C ~/OpenSrc/nerd-fonts
 
     mkdir -p ~/.local/share/fonts/NerdFonts
-    cp  ~/OpenSrc/nerd-fonts/*.ttf ~/.local/share/fonts/NerdFonts/
+    cp ~/OpenSrc/nerd-fonts/*.ttf ~/.local/share/fonts/NerdFonts/
     fc-cache -fv
     echo "Notice: Modify Terminal fonts to Nerd-Fonts"
   fi
-fi
-
-if [ $CMD = "pack" ]; then
-  set -euo pipefail
-
-  # 源路径（你的本地配置目录和插件目录）
-  NVIM_CONFIG="${HOME}/.config/nvim"
-  NVIM_LAZY="${HOME}/.local/share/nvim/lazy"
-
-  # 输出文件名
-  OUTPUT="nvim_lazy_bundle.tar.gz"
-
-  # 确保 lazy-lock.json 存在（记录插件版本）
-  if [[ ! -f "${NVIM_CONFIG}/lazy-lock.json" ]]; then
-    echo "lazy-lock.json 不存在，请先在 nvim 里执行 :Lazy sync"
-    exit 1
-  fi
-
-  # 打包配置和插件
-  echo "正在打包 Neovim 配置和插件..."
-  tar -czf "${OUTPUT}" \
-    -C "${HOME}/.config" nvim \
-    -C "${HOME}/.local/share/nvim" lazy
-
-  echo "打包完成: ${OUTPUT}"
-fi
-
-if [ $CMD = "unpack" ]; then
-  tar -xzf nvim_lazy_bundle.tar.gz -C ~/
 fi
